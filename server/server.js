01dd8834f059cc6cc3088cb38d432c22a85adaa5
Yarden Sibony <yardensibonyy@gmail.com>
2018-02-12 13:35:24 +0200

    g

--- server/server.js
+++ server/server.js
@@ -14,12 +14,9 @@ mongoose.connect(keys.mongoURI);
 require('./routes/apiRoutes')(app);
 
 if(process.env.NODE_ENV === 'production') {
-    //Express will serve up production assests like main.js or main.css file.
     app.use(express.static('client/build'));
-
-    //Express will serve up index.html file if it doesn't recognize the route (e.g react route)
+    
     app.get('/*', (req, res) => {
-        //res.send({bodt: "Gdfdfg"});
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
