2ea82f417bae8497ff00e55831177f7d1cb91086
Yarden Sibony <yardensibonyy@gmail.com>
2018-02-12 15:34:44 +0200

    g

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ require('./routes/apiRoutes')(app);
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    app.get('/*', (req, res) => {
+    app.get('*', (req, res) => {
         res.setHeader('content-type', 'text/html');
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
